d5ce1e7367cc53d95177f1d7d0a949174ef07be2
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-05-06 21:36:16 +0700

    adjust Hero font style

--- Components/Hero.js
+++ Components/Hero.js
@@ -3,9 +3,9 @@
 export default function Hero() {
   return (
     <div>
-      <h1 className="font-oleo font-semibold italic text-6xl text-white pt-14 text-center leading-normal  md:text-left md:text-9xl md:pl-[150px]">
+      <h1 className="font-libre font-400 text-6xl text-white pt-14 text-center leading-normal  md:text-left md:text-9xl md:pl-[150px]">
         Welcome <br /> to
-        <p className="font-dancing bg-gradient-to-r from-rose-400 to-orange-300 bg-clip-text text-transparent font-bold">4CG Store</p>
+        <p className="font-dancing bg-gradient-to-r from-rose-400 to-orange-300 bg-clip-text text-transparent font-bold md:h-36">4CG Store</p>
         <a
           href="./product"
           className="text-lg text-white border-2 border-solid rounded-lg p-2 hover:bg-white hover:text-black font-semibold transition duration-75 ease-linear"
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
     fontFamily: {
       'Poppins': ["Poppins", "sans-serif"],
-      'libre': ['"Libre Baskerville"', 'serif'],
-      'oleo': ['"Oleo Script"', 'cursive'],
+      'libre': 'Libre\\ Baskerville, serif',
+      'oleo': 'Oleo\\ Script, cursive',
       'satisfy': ['Satisfy', 'cursive'],
       'playfair': ['"Playfair Display"', 'serif'],
       'dancing': ['"Dancing Script"', 'cursive']
     },
+    extend: {},
   },
   plugins: [],
 };
