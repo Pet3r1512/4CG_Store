4ac28e99a80f70c21a820251eed335344400329a
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-05-06 21:46:21 +0700

    change hero font style

--- Components/Hero.js
+++ Components/Hero.js
@@ -3,12 +3,12 @@
 export default function Hero() {
   return (
     <div>
-      <h1 className="font-libre font-400 text-6xl text-white pt-14 text-center leading-normal  md:text-left md:text-9xl md:pl-[150px]">
+      <h1 className="font-playfair italic font-400 text-6xl text-white pt-14 text-center leading-normal  md:text-left md:text-9xl md:pl-[150px]">
         Welcome <br /> to
-        <p className="font-dancing bg-gradient-to-r from-rose-400 to-orange-300 bg-clip-text text-transparent font-bold md:h-36">4CG Store</p>
+        <p className="font-dancing not-italic bg-gradient-to-r from-rose-400 to-orange-300 bg-clip-text text-transparent font-bold md:h-36">4CG Store</p>
         <a
           href="./product"
-          className="text-lg text-white border-2 border-solid rounded-lg p-2 hover:bg-white hover:text-black font-semibold transition duration-75 ease-linear"
+          className="font-libre not-italic text-lg text-white border-2 border-solid rounded-lg p-2 hover:bg-white hover:text-black font-semibold transition duration-200 ease-linear"
         >
           SHOP NOW!
         </a>
