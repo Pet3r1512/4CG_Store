f6b7eeae0c7fc53692e240f25ba48f98ff73ba8b
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-05-21 22:46:29 +0700

    adjust responsive on home/about page.

--- Components/Hero.js
+++ Components/Hero.js
@@ -3,14 +3,16 @@
 export default function Hero() {
   return (
     <div>
-      <h1 className="font-playfair italic font-400 text-6xl text-white pt-14 text-center leading-normal  md:text-left md:text-9xl md:pl-[150px]">
-        Welcome <br /> to
-        <p className="font-dancing not-italic bg-gradient-to-r from-rose-400 to-orange-300 bg-clip-text text-transparent font-bold md:h-36">
-          4CG Store
-        </p>
+      <h1 className="text-white h-screen flex flex-col items-center justify-start mt-14 gap-y-10">
+        <div className="font-libre text-6xl sm:text-7xl text-center italic md:text-8xl">
+          Welcome <br /> to
+          <p className="font-dancing not-italic bg-gradient-to-r from-rose-400 to-orange-300 bg-clip-text text-transparent font-bold md:h-36 w-full px-2">
+            4CG Store
+          </p>
+        </div>
         <a
           href="./product"
-          className="font-libre not-italic text-lg text-white border-2 border-solid rounded-lg p-2 hover:bg-white hover:text-black font-semibold transition duration-200 ease-linear"
+          className="font-playfair not-italic text-sm md:text-lg text-white border-2 border-solid rounded-lg p-2 hover:bg-white hover:text-black font-semibold transition duration-200 ease-linear"
         >
           SHOP NOW!
         </a>
