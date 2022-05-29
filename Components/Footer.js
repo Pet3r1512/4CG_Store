5b3a82bfd87e23298993762525a08262123def82
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-05-29 21:36:29 +0700

    build responsive footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white">
       <div className="flex items-center py-4 max-w-7xl mx-auto">
-        <div className="w-full grid grid-cols-1 col-auto">
-          <div className="sm:flex sm:gap-x-3 hidden">
+        <div className="w-full grid grid-cols-3">
+          <div className="sm:flex sm:items-center sm:gap-x-2 hidden font-semibold sm:text-sm">
             <Link href="./about">
               <a href="">About 4CG</a>
             </Link>
@@ -14,14 +14,13 @@ export default function Footer() {
               <a href="">Return Policy</a>
             </Link>
           </div>
-          <div className="flex gap-x-1 justify-center">
+          <div className="flex gap-x-1 justify-center items-center w-screen sm:w-full">
             <Copyright></Copyright>
-            <p>2022 4CG, LLC. All Rights Reserved.</p>
+            <p className="font-semibold sm:text-sm">
+              2022 4CG, LLC. All Rights Reserved.
+            </p>
           </div>
-          <div className="sm:flex sm:gap-x-3 sm:justify-end hidden">
-            <Link href="">
-              <a href="">Contact us</a>
-            </Link>
+          <div className="h-full sm:flex sm:items-center sm:gap-x-2 sm:justify-end hidden font-semibold">
             <Link href="">
               <Facebook></Facebook>
             </Link>
