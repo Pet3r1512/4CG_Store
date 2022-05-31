0f1e3f40ea82d94c6b7237beab3213493ccb0522
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-05-31 15:09:14 +0700

    add policy page & minor changes in about page

--- pages/about.js
+++ pages/about.js
@@ -23,29 +23,29 @@ export default function About() {
   const about_content2 = `Đồng thời 4CG xin bày tỏ lòng cảm ơn sự quan tâm và đồng hành của các G-er trong suốt thời gian phát triển của chúng mình. Chúng mình sẽ luôn hoàn thiện và đổi mới từng ngày để mang đến cho các bạn những sản phẩm tuyệt vời hơn nữa, from 4CG with love!`;
   return (
     <div
-      className="bg-cover fixed w-full h-screen overflow-auto"
+      className="bg-cover fixed w-full h-screen overflow-auto flex flex-col"
       style={{ backgroundImage: `url("images/aboutUs.jpg")` }}
     >
-      <SideBar />
-      <div className="">
+      <div>
+        <SideBar />
         <Header />
         <SubSearchBar />
-        <div className="px-6 py-10 h-screen text-[#383838] md:py-0">
-          <div className="flex flex-col h-full md:w-full items-center justify-start gap-y-10 md:justify-center md:gap-y-14">
-            <h1 className="text-3xl sm:text-5xl font-libre font-bold md:text-6xl">
-              About Us
-            </h1>
-            <div className="leading-relaxed font-libre text-sm sm:text-sm md:text-lg lg:text-xl md:leading-loose flex flex-col max-w-7xl mx-auto">
-              <p>{about_content1}</p>
-              <br />
-              <p>{about_content2}</p>
-            </div>
-            <div className="border border-[#1C0A00] rounded-lg p-4 cursor-pointer">
-              <q>FASHION HAS NO LIMIT</q>
-            </div>
-            <div className="flex justify-evenly w-2/4 md:w-1/4">
-              <Social_Logos />
-            </div>
+      </div>
+      <div className="px-6 py-10 h-screen text-[#383838] md:py-0">
+        <div className="flex flex-col h-full md:w-full items-center justify-start gap-y-10 md:justify-center md:gap-y-14">
+          <h1 className="text-3xl sm:text-5xl font-libre font-bold md:text-6xl">
+            About Us
+          </h1>
+          <div className="leading-relaxed font-libre text-sm sm:text-sm md:text-lg lg:text-xl md:leading-loose flex flex-col max-w-7xl mx-auto">
+            <p>{about_content1}</p>
+            <br />
+            <p>{about_content2}</p>
+          </div>
+          <div className="border border-[#1C0A00] rounded-lg p-4 cursor-pointer">
+            <q>FASHION HAS NO LIMIT</q>
+          </div>
+          <div className="flex justify-evenly w-2/4 md:w-1/4 md:hidden">
+            <Social_Logos />
           </div>
         </div>
       </div>
--- pages/policy.js
+++ pages/policy.js
@@ -5,14 +5,17 @@ import SubSearchBar from "../Components/SubSearchBar"
 
 export default function Policy (){
    return (
-      <div>
+      <div className="h-screen flex flex-col justify-between">
          <div className="w-full z-10 top-0 bg-gray-600 pb-5">
             <Header />
             <SideBar />
             <SubSearchBar />
          </div>
-         <div className="h-screen">
-            
+         <div className="flex flex-col items-center gap-y-14 p-6 font-playfair text-xl md:items-start">
+            <h1 className="text-3xl font-libre">OUR RETURN POLICY</h1>
+            <p>&bull; 4CG accepts returns of unworn, unwashed and undamaged product for full refund or exchange within 14 days of original purchase. Items received after 14 days will be refunded via store credit.</p>
+            <p>&bull; Customer is responsible for any additional shipping costs. Return labels will not be provided.</p>
+            <p>&bull; If you need to return an items, please contact us with your order number at <strong>4CGstore@gmail.com</strong></p>
          </div>
          <Footer />
       </div>
