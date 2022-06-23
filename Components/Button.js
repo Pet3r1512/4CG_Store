84c120b2c5dc38adff6d44904f51280c2cc179a7
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-06-23 21:29:39 +0700

    add default text for Button

---
+++ Components/Button.js
@@ -0,0 +1,11 @@
+export default function Button(props) {
+  return (
+    <button className="bg-red-400 text-white font-semibold text-xl px-6 py-1 rounded-lg flex justify-center items-center border-[3px] border-red-400 hover:text-red-500 hover:bg-white transition duration-100 ease-linear font-dancing max-w-[150px]">
+      {props.text}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  text: "Add to cart",
+};
