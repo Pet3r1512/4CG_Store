f3a25624674fb953c9d1d491e767e1d5e1a5c372
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-06-23 21:28:42 +0700

    Change Button component

--- Components/Card.js
+++ Components/Card.js
@@ -1,10 +1,10 @@
-/* eslint-disable @next/next/link-passhref */
-/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @next/next/link-passhref,@next/next/no-img-element */
 import Link from "next/link";
+import Button from "./Button";
 
 export default function Card(props) {
   return (
-    <div className="mx-auto mt-4 card px-6 pb-4 flex flex-col gap-4 items-center">
+    <div className="mx-auto mt-4 card px-6 pb-4 flex flex-col gap-2 items-center">
       <div className="min-h-[550px] flex items-center">
         <img
           src={props.img}
@@ -16,9 +16,9 @@ export default function Card(props) {
         <h1 className="font-libre text-3xl">{props.name}</h1>
         <p className="font-poppins">{props.price} VND</p>
       </div>
-      <div className="">
-        <Link href={`/products/${props.slug}`}>
-          <button className="bg-red-400 text-white font-semibold text-xl px-6 py-1 rounded-lg flex justify-center items-center border-[3px] border-red-400 hover:text-red-500 hover:bg-white transition duration-100 ease-linear font-dancing">
+      <div>
+        <Link href={`/products/${props.slug}`} passHref>
+          <button className="bg-red-400 text-white font-semibold text-xl px-6 py-1 rounded-lg flex justify-center items-center border-[3px] border-red-400 hover:text-red-500 hover:bg-white transition duration-100 ease-linear font-dancing max-w-[150px]">
             Buy now !
           </button>
         </Link>
