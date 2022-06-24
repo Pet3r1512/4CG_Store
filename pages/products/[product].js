eb57b452c0f39cbb450b7bcc489099eea71e1123
Pet3r1512 <89619124+Pet3r1512@users.noreply.github.com>
2022-06-24 21:13:52 +0700

    price formatting

--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,9 +1,11 @@
 import { useAppContext } from "../../hooks/state";
 import SideBar from "../../Components/SideBar";
 import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
 import SubSearchBar from "../../Components/SubSearchBar";
 import Button from "../../Components/Button";
-import { prisma } from '../../backend/getPrismaClient';
+import { convertPrice } from "../../hooks/formatPrice";
+import { prisma } from "../../backend/getPrismaClient";
 
 export async function getServerSideProps(context) {
   const slug = context.query.product;
@@ -12,7 +14,7 @@ export async function getServerSideProps(context) {
 
   const product = await prisma.product.findUnique({
     where: { key },
-  })
+  });
 
   return {
     props: {
@@ -28,28 +30,35 @@ export default function Product({ product }) {
   const addToCartHandler = () => addToCart(product);
 
   return (
-    <div>
-      <SideBar />
-      <div className="w-full z-10 top-0 bg-gray-600 pb-5" id="navbar">
-        <Header />
-        <SubSearchBar />
-      </div>
-      <div className="max-w-7xl mx-auto">
-        <div className="my-20 w-full flex flex-col sm:flex-row sm:justify-end">
-          <div className="max-w-[350px] sm:max-w-[450px] md:max-w-[500px] mx-auto">
-            <img src={product.img} alt=""/>
-          </div>
-          <div className="mx-[30px] sm:mx-auto w-full flex flex-col gap-y-5">
-            <div className="flex flex-col gap-y-1">
-              <h1 className="font-poppins font-bold text-2xl">
-                {product.name}
-              </h1>
-              <p className="font-semibold text-lg">{product.price}</p>
+    <>
+      <div>
+        <SideBar />
+        <div className="w-full z-10 top-0 bg-gray-600 pb-5" id="navbar">
+          <Header />
+          <SubSearchBar />
+        </div>
+        <div className="max-w-7xl mx-auto">
+          <div className="my-20 w-full flex flex-col sm:flex-row sm:justify-end">
+            <div className="max-w-[350px] sm:max-w-[450px] md:max-w-[500px] mx-auto">
+              <img src={product.img} alt="" />
             </div>
-            <Button onClick={addToCartHandler} />
+            <div className="mx-[30px] sm:mx-auto w-full flex flex-col gap-y-5">
+              <div className="flex flex-col gap-y-1">
+                <h1 className="font-poppins font-bold text-2xl">
+                  {product.name}
+                </h1>
+                <p className="font-semibold text-lg">
+                  {(product.price = convertPrice(product.price))}
+                </p>
+              </div>
+              <Button onClick={addToCartHandler} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="relative">
+        <Footer />
+      </div>
+    </>
   );
 }
